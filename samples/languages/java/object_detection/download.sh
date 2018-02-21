948e919b9c3c8d441863acda368ad2e6b5650c10
conbitin <conbitin@gmail.com>
2018-02-21 17:13:10 +0700

    Ignore ssl cert verification

--- samples/languages/java/object_detection/download.sh
+++ samples/languages/java/object_detection/download.sh
@@ -9,10 +9,10 @@ cd "${DIR}"
 # From:
 # https://github.com/tensorflow/models/blob/f87a58c/research/object_detection/protos/string_int_label_map.proto
 mkdir -p src/main/protobuf
-curl -L -o src/main/protobuf/string_int_label_map.proto "https://raw.githubusercontent.com/tensorflow/models/f87a58cd96d45de73c9a8330a06b2ab56749a7fa/research/object_detection/protos/string_int_label_map.proto"
+curl -L -k -o src/main/protobuf/string_int_label_map.proto "https://raw.githubusercontent.com/tensorflow/models/f87a58cd96d45de73c9a8330a06b2ab56749a7fa/research/object_detection/protos/string_int_label_map.proto"
 
 # Labels from:
 # https://github.com/tensorflow/models/tree/865c14c/research/object_detection/data
 mkdir -p labels
-curl -L -o labels/mscoco_label_map.pbtxt "https://raw.githubusercontent.com/tensorflow/models/865c14c1209cb9ae188b2a1b5f0883c72e050d4c/research/object_detection/data/mscoco_label_map.pbtxt"
-curl -L -o labels/oid_bbox_trainable_label_map.pbtxt "https://raw.githubusercontent.com/tensorflow/models/865c14c1209cb9ae188b2a1b5f0883c72e050d4c/research/object_detection/data/oid_bbox_trainable_label_map.pbtxt"
+curl -L -k -o labels/mscoco_label_map.pbtxt "https://raw.githubusercontent.com/tensorflow/models/865c14c1209cb9ae188b2a1b5f0883c72e050d4c/research/object_detection/data/mscoco_label_map.pbtxt"
+curl -L -k -o labels/oid_bbox_trainable_label_map.pbtxt "https://raw.githubusercontent.com/tensorflow/models/865c14c1209cb9ae188b2a1b5f0883c72e050d4c/research/object_detection/data/oid_bbox_trainable_label_map.pbtxt"
