88c8eb3ded5bcec9b0ad07a43215d0765d979ffa
duc.quynh <duc.quynh>
2018-02-21 16:48:49 +0700

    Ignore ssl certificate verification & fix whale.jpg error when downloading

--- samples/languages/java/label_image/download_sample_images.sh
+++ samples/languages/java/label_image/download_sample_images.sh
@@ -4,7 +4,7 @@ mkdir -p ${DIR}/images
 cd ${DIR}/images
 
 # Some random images
-curl -o "porcupine.jpg" -L "https://cdn.pixabay.com/photo/2014/11/06/12/46/porcupines-519145_960_720.jpg"
-curl -o "whale.jpg" -L "https://static.pexels.com/photos/417196/pexels-photo-417196.jpeg"
-curl -o "terrier1u.jpg" -L "https://upload.wikimedia.org/wikipedia/commons/3/34/Australian_Terrier_Melly_%282%29.JPG"
-curl -o "terrier2.jpg" -L "https://cdn.pixabay.com/photo/2014/05/13/07/44/yorkshire-terrier-343198_960_720.jpg"
+curl -o "porcupine.jpg" -L -k "https://cdn.pixabay.com/photo/2014/11/06/12/46/porcupines-519145_960_720.jpg"
+curl -o "whale.jpg" -L -k "http://static.pexels.com/photos/417196/pexels-photo-417196.jpeg"
+curl -o "terrier1u.jpg" -L -k "https://upload.wikimedia.org/wikipedia/commons/3/34/Australian_Terrier_Melly_%282%29.JPG"
+curl -o "terrier2.jpg" -L -k "https://cdn.pixabay.com/photo/2014/05/13/07/44/yorkshire-terrier-343198_960_720.jpg"
